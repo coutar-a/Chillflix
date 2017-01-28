1b8964f00f2883128901f153b99a6fe8774e337c
Nadia <nadia.chicoine@hotmail.com>
2017-01-28 13:15:16 -0500

    ajout de la page series

--- js/pageLoader.js
+++ js/pageLoader.js
@@ -7,17 +7,26 @@ function loadHomePage() {
     $homePage.removeClass('hide');
     $actorPage.addClass('hide');
     $moviePage.addClass('hide');
+    $seriesPage.addClass('hide');
 }
 
 function loadMoviePage() {
     $homePage.addClass('hide');
     $actorPage.addClass('hide');
     $moviePage.removeClass('hide');
+    $seriesPage.addClass('hide');
 }
 
 function loadActorPage() {
-    console.log("allo")
     $homePage.addClass('hide');
     $actorPage.removeClass('hide');
     $moviePage.addClass('hide');
+    $seriesPage.addClass('hide');
+}
+
+function loadSeriesPage() {
+    $homePage.addClass('hide');
+    $actorPage.addClass('hide');
+    $moviePage.addClass('hide');
+    $seriesPage.removeClass('hide');
 }
