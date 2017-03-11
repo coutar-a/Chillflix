cb5aee86b93ed3c9fa982fe574fda45069b08ece
Ambroise Coutarel <AGCoutarel@gmail.com>
2017-03-11 17:48:05 -0500

    Now getting an actor photo from IMDB's undocumented API. You need to click to refresh the page, tho.

--- js/models/ActorModel.js
+++ js/models/ActorModel.js
@@ -8,22 +8,41 @@
                 headers: {
                     'Authorization': userProfile.token
                 }}).success(function(_data, success, result){
-                console.log("actor", result.responseJSON);
-                self.artistName = result.responseJSON.results[0].artistName;
-                self.primaryGenreName = result.responseJSON.results[0].primaryGenreName;
-                self.artistLinkUrl = result.responseJSON.results[0].artistLinkUrl;
+                    console.log("actor", result.responseJSON);
+                    self.artistName = result.responseJSON.results[0].artistName;
+                    self.primaryGenreName = result.responseJSON.results[0].primaryGenreName;
+                    self.artistLinkUrl = result.responseJSON.results[0].artistLinkUrl;
 
-            })
-        },
-        fetchImage: function(id){
-            this.url = "https://jsonplaceholder.typicode.com/photos/" + id;
-            var self = this;
+                })
+            },
+            fetchImage: function(id){
+                this.url = 'http://imdb.wemakesites.net/api/search?q=' + this.artistName + '&api_key=388d1563-41f9-4179-bd55-965e21e45bf1';
+                var self = this;
+                var actorData;
+                var image;
+            //we need to chain 2 consecutive ajax calls to get the actor picture, because this is 2017 and we live in the worst cyberpunk future possible
+
+            return this.fetch({dataType: 'jsonp'}).success(function(_data, success, result){
+
+                console.log(_data.data.results.names);
+                //console.log(self.artistName);
+                actorData = _data.data.results.names.find((a) => a.title == self.artistName);
+                console.log(actorData);
+            }).then(function() {
+
+                $.ajax({
+                    url: 'http://imdb.wemakesites.net/api/' + actorData.id + '?api_key=388d1563-41f9-4179-bd55-965e21e45bf1',
+                    type: 'GET',
+                    dataType: 'jsonp'
+                })
+                .done(function(data) {
+                    console.log(data.data.image);
+                    self.artistImage = data.data.image;
+                })
+
+            });
+
             
-            return this.fetch().success(function(_data, success, result){
-                console.log("image", result.responseJSON);
-                self.artistImage = result.responseJSON.thumbnailUrl;
-
-            })
         }
         
     });
@@ -31,3 +50,17 @@
     actorModel = new ActorModel();
 
 })(jQuery);
+
+/* $.ajax({
+                url: 'http://imdb.wemakesites.net/api/search?q=' + actor.artistName + '&api_key=388d1563-41f9-4179-bd55-965e21e45bf1',
+                type: 'GET'
+            })
+            .done(function(data) {
+                console.log("success");
+                console.log(data);
+
+                console.log(actorData);
+            })
+            .fail(function() {
+                console.log("error");
+            });*/
